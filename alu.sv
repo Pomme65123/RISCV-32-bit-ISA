065be0035fbe04520216c7cd9920afc7d2ab1c41
RovasFlow <shan65123@gmail.com>
2026-01-04 15:16:05 -0500

    Starting ALU.sv

--- alu.sv
+++ alu.sv
@@ -1,2 +1,18 @@
-module alu ();
+`timescale 1ns/1ps
+
+module alu (
+    input logic [31:0] a,
+    input logic [31:0] b,
+    input logic [3:0] alu_control,
+    output logic [31:0] result,
+    output logic zero
+);
+
+    // ALU Control Codes
+
+    // Case Operations
+
+    // Branch Logic
+
+
 endmodule
