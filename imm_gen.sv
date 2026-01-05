637a6833e981e54edc722a574220c9c4d6598235
RovasFlow <shan65123@gmail.com>
2026-01-05 11:15:26 -0500

    Beginning imm_gen.sv

--- imm_gen.sv
+++ imm_gen.sv
@@ -1,2 +1,16 @@
-module imm_gen.sv ();
+`timescale 1ns/1ps
+
+module imm_gen.sv (
+    input  logic [31:0] instr,
+    output logic [31:0] imm
+);
+
+    // OPCODES
+
+    /* 
+        Combinational logic for
+        OP_LUI, OP_AUIPC, OP_JAL, OP_JALR,
+         OP_BRANCH, OP_LOAD, OP_STORE, OP_IMM
+    */
+
 endmodule
