36d17c6abb471030b524441728cf1548612415c9
RovasFlow <shan65123@gmail.com>
2026-01-04 21:58:08 -0500

    Starting data_memory.sv

--- data_memory.sv
+++ data_memory.sv
@@ -1,2 +1,23 @@
-module data_memory();
+module data_memory #(
+    parameter ADDR_WIDTH = 32,
+    parameter DATA_WIDTH = 32,
+    parameter MEM_SIZE   = 1024
+) (
+    input  logic                    clk,
+    input  logic [ADDR_WIDTH-1:0]   addr,
+    input  logic [DATA_WIDTH-1:0]   wdata,
+    input  logic                    we,
+    input  logic [3:0]              be,
+    output logic [DATA_WIDTH-1:0]   rdata
+);
+
+    // Create Memory
+
+    // Initialize Memory
+
+    // Create Write
+
+    // Create Read
+
+
 endmodule
